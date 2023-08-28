8c17e73d022debd2e52b8a6bad66d9b4f27c4d86
kundi21 <cundi.machado@gmail.com>
2023-08-28 11:20:18 -0700

    task 0

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+task 0. Basic annotations - add
+Write a type-annotated function add that takes a float a and a float b
+as arguments and returns their sum as a float.
+"""
+import typing
+
+def add(a: float, b: float) -> float:
+    """Return the sum of a and b."""
+    return a + b
