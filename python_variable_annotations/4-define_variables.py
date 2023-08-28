d21093cb49bfc5f8bfee6c8383d6e1029102ec2e
kundi21 <cundi.machado@gmail.com>
2023-08-28 11:58:46 -0700

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+task 4. Define variables
+Define and annotate the following variables with the specified values:
+-------------------------------
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of “Holberton”
+"""
+import typing
+
+def define_variables() -> typing.Tuple[int, float, bool, str]:
+    """
+    Define variables.
+    """
+    a: int = 1
+    pi: float = 3.14
+    i_understand_annotations: bool = True
+    school: str = "Holberton"
+    return a, pi, i_understand_annotations, school
