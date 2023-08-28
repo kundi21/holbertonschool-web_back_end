3809cd6a51f5afa8d61fef4f7cb347ecec994cb8
kundi21 <cundi.machado@gmail.com>
2023-08-28 13:06:28 -0700

    pycodestyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -8,6 +8,7 @@ def element_length(lst):
 """
 from typing import Iterable, List, Tuple, Sequence
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """return [(i, len(i)) for i in lst]
     """
