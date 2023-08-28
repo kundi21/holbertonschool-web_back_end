b28da47f9dfcb30317477cbff0b56a21a8333bdb
kundi21 <cundi.machado@gmail.com>
2023-08-28 12:50:39 -0700

    9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+task 9. Let's duck type an iterable object
+Annotate the below functions parameters and
+return values with the appropriate types.
+def element_length(lst):
+    return [(i, len(i)) for i in lst]
+"""
+from typing import Iterable, List, Tuple
+
+
+def element_length(lst: Iterable[str]) -> List[Tuple[str, int]]:
+    """
+    return [(i, len(i)) for i in lst]
+    """
+    return [(i, len(i)) for i in lst]
