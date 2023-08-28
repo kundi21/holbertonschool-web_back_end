8f1053fc675faa930b2f678d63461ce0f5420df7
kundi21 <cundi.machado@gmail.com>
2023-08-28 11:26:02 -0700

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+task 1. Basic annotations - concat
+Write a type-annotated function concat that takes a
+string str1 and a string str2
+as arguments and returns a concatenated string.
+"""
+import typing
+
+
+def concat(str1: str, str2: str) -> str:
+    return str1 + str2
